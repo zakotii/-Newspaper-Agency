709c0037850df5124679ab8a82eee86baf765e38
Юрий <you@exampleabc1234567892777@gmail.com>
2024-10-20 22:16:32 +0300

    added decorators and class-based views, LoginRequiredMixin

--- Newspaper_Agency/urls.py
+++ Newspaper_Agency/urls.py
@@ -1,33 +1,34 @@
-"""Newspaper_Agency URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from . import views
 from .views import home
 from django.contrib.auth import views as auth_views
 
-
 urlpatterns = [
+    # Главная страница
     path("", home, name="home"),
+
+    # Админка
     path('admin/', admin.site.urls),
+
+    # Стандартные пути для авторизации/регистрации через Django
     path('accounts/', include('django.contrib.auth.urls')),
-    path('newspapers/', views.newspaper_list, name='newspaper_list'),
+
+    # Список газет
+    path('newspapers/', views.NewspaperListView.as_view(), name='newspaper_list'),
+
+    # Создание новой газеты
     path('newspaper/create/', views.newspaper_create, name='newspaper_create'),
+
+    # Обновление газеты
     path('newspaper/<int:pk>/update/', views.newspaper_update, name='newspaper_update'),
+
+    # Список редакторов
     path('redactors/', views.redactor_list, name='redactor_list'),
+
+    # Список тем
     path('topics/', views.topic_list, name='topic_list'),
+
+    # Logout с перенаправлением на главную страницу
     path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
-    ]
+]
--- Newspaper_Agency/views.py
+++ Newspaper_Agency/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib import messages
-from newspaper_app.models import Newspaper
+from newspaper_app.models import Newspaper, Redactor, Topic  # Удалено дублирование
 from newspaper_app.forms import NewspaperForm
-from newspaper_app.models import Newspaper, Redactor, Topic
+from django.contrib.auth.decorators import login_required  # Для декораторов
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView
 
 
 def home(request):
@@ -21,11 +23,19 @@ def home(request):
     return render(request, 'home.html', context)
 
 # Список газет
+@login_required  # Добавлен декоратор
 def newspaper_list(request):
     newspapers = Newspaper.objects.all()
     return render(request, 'newspaper_list.html', {'newspapers': newspapers})
 
+# Классовое представление с ограничением доступа
+class NewspaperListView(LoginRequiredMixin, ListView):
+    model = Newspaper
+    template_name = 'newspaper_list.html'
+    context_object_name = 'newspapers'  # Убедитесь, что используете это имя в шаблоне
+
 # Создание новой газеты
+@login_required  # Добавлен декоратор
 def newspaper_create(request):
     if request.method == "POST":
         form = NewspaperForm(request.POST)
@@ -40,6 +50,7 @@ def newspaper_create(request):
     return render(request, 'newspaper_form.html', {'form': form})
 
 # Обновление газеты
+@login_required  # Добавлен декоратор
 def newspaper_update(request, pk):
     newspaper = get_object_or_404(Newspaper, pk=pk)
     if request.method == "POST":
@@ -54,11 +65,14 @@ def newspaper_update(request, pk):
         form = NewspaperForm(instance=newspaper)
     return render(request, 'newspaper_form.html', {'form': form})
 
-
+# Список редакторов
+@login_required  # Добавлен декоратор
 def redactor_list(request):
     redactors = Redactor.objects.all()
     return render(request, 'redactor_list.html', {'redactors': redactors})
 
+# Список тем/статей
+@login_required  # Добавлен декоратор
 def topic_list(request):
     topics = Topic.objects.all()  # Получаем список тем
     return render(request, 'topic_list.html', {'topics': topics})
